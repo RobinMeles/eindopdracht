d988a2669472908f6afd7b06934946d82b0510db
RobinMeles <robinmeles@gmail.com>
2018-07-08 22:53:00 +0200

    installation of salt master

---
+++ salt-master.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+#installing salt master
+
+apt-get update
+apt-get upgrade -y
+
+wget -O - https://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest/SALTSTACK-GPG-KEY.pub | sudo apt-key add -
+
+echo deb http://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest xenial main >> /etc/apt/sources.list.d/saltstack.list
+sudo apt-get update
+
+sudo apt-get install salt-master -Y
+
+sudo systemctl restart salt-master
+
+sudo sed -i 's/#interface 0.0.0.0/interface 10.0.0.4/' /etc/salt/master
+
+salt-key -A
