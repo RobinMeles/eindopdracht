f2467a7e5f6d52c08f8cab77c7c3cf3d6b600749
RobinMeles <robinmeles@gmail.com>
2018-07-11 11:53:37 +0200

    Update remove-master.sh

--- remove-master.sh
+++ remove-master.sh
@@ -3,15 +3,14 @@
 mysql --user="root" --password="root" --database="cacti" -e "DROP DATABASE cacti;"
 
 salt-key -d '*'
+apt purge syslog-ng* -y
 apt purge apache2* -y
 apt purge mysql* -y
 apt purge php* -y
 apt purge salt* -y
-apt purge rsyslog-mysql -y
 apt purge snmp* -y
-apt purge zip -y
-apt purge unzip -y
-apt purge rddtool -y
+apt purge snmpd* -y
+apt purge cacti* -y
 rm -r /var/www/html/
 rm -r /etc/php/
 rm -r /var/lib/php/
@@ -19,7 +18,7 @@ rm -r /srv/salt/
 rm -r /etc/salt/
 rm -r /var/log/salt/
 rm -r /var/cache/salt/
-rm -r /home/andres/cacti*
+rm -r /etc/syslog-ng
 
 apt-get purge docker-ce -y
 rm -rf /var/lib/docker
