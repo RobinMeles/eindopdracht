7ea3525b4503f36e93c40da917cbec51bb9c4fa8
RobinMeles <robinmeles@gmail.com>
2018-07-11 11:55:17 +0200

    Update remove-minion.sh

--- remove-minion.sh
+++ remove-minion.sh
@@ -4,6 +4,7 @@ apt purge apache2* -y
 apt purge mysql* -y
 apt purge php* -y
 apt purge snmp* -y
+apt purge snmpd* -y
 apt purge salt* -y
 rm -r /var/lib/apache2/
 rm -r /var/www/html/
@@ -12,6 +13,5 @@ rm -r /var/lib/php/
 rm -r /etc/salt/
 rm -r /var/log/salt/
 rm -r /var/cache/salt/
+rm -r /etc/syslog-ng
 
-apt-get remove salt-minion
-apt-get purge salt-minion
