e3724f0fdc91167e4cff10a0a2fbae92dfc4ff37
RobinMeles <robinmeles@gmail.com>
2018-07-11 14:07:05 +0200

    Update finalscript.sh

--- finalscript.sh
+++ finalscript.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+#Auteur: Robin Meles (studentnummer 283873)
+#Datum: 11 juli 2018
 
 #--SALT-MASTER--
 
@@ -20,7 +22,9 @@ salt-key -A
 
 salt-master
 
-# Installeren van cacti op monitor/master
+#---CACTI---
+
+#Installeren van cacti op monitor/master
 
 apt-get update
 apt-get upgrade -y
@@ -36,7 +40,7 @@ apt-get install php5-snmp -y
 
 apt-get install cacti -y
 
-# Installervan snmp/snmpd voor cacti op minion
+#Installervan snmp/snmpd voor cacti op minion
 
 salt 'Ubu1604-Minion' cmd.run 'apt-get install snmp -y'
 salt 'Ubu1604-Minion' cmd.run 'apt-get install snmpd -y'
@@ -95,3 +99,69 @@ EOF'
 
 salt 'Ubu1604-Minion' cmd.run 'sudo systemctl start syslog-ng'
 salt 'Ubu1604-Minion' cmd.run 'sudo systemctl enable syslog-ng'
+
+#---DOCKER---
+
+#Installation of docker according to https://docs.docker.com/install/linux/docker-ce/ubuntu/#install-docker-ce-1
+
+sudo apt-get update
+
+#Install packages to allow apt to use a repository over HTTPS
+sudo apt-get install \
+apt-transport-https \
+ca-certificates \
+curl \
+software-properties-common
+
+#Add Docker’s official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+# Verify key matches fingerprint: 9DC8 5822 9FC7 DD38 854A E2D8 8D81 803C 0EBF CD88
+#apt-key fingerprint 0EBFCD88
+
+#Setting up the stable repository.
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+
+#Installing Docker CE
+sudo apt-get install docker-ce -y
+
+#Verifying that Docker CE is installed correctly by running the hello-world image
+sudo docker run hello-world
+
+#---WORDPRESS---
+
+#Installing Wordpress according to https://www.techrepublic.com/article/how-to-install-wordpress-on-ubuntu-16-04/
+
+wget -c http://wordpress.org/latest.tar.gz
+tar -xzvf latest.tar.gz
+
+#move the entire wordpress folder to /var/www/html to make sure other services can be hosted
+sudo rsync -av wordpress/* /var/www/html/
+
+#Giving permissions
+sudo chown -R www-data:www-data /var/www/html/
+sudo chmod -R 755 /var/www/html/
+
+#Restarting apache and mysql
+sudo systemctl restart apache2.service
+sudo systemctl restart mysql.service
+
+#---WORDPRESS MINION---
+
+#Installing Wordpress on the minion according to the same installation guide
+
+salt 'Ubu1604-Minion' cmd.run 'wget -c http://wordpress.org/latest.tar.gz' 
+salt 'Ubu1604-Minion' cmd.run 'tar -xzvf latest.tar.gz' 
+
+salt 'Ubu1604-Minion' cmd.run 'sudo rsync -av wordpress/* /var/www/html/' 
+
+salt 'Ubu1604-Minion' cmd.run 'sudo chown -R www-data:www-data /var/www/html/' 
+salt 'Ubu1604-Minion' cmd.run 'sudo chmod -R 755 /var/www/html/' 
+
+salt 'Ubu1604-Minion' cmd.run 'sudo systemctl restart apache2.service'
+salt 'Ubu1604-Minion' cmd.run 'sudo systemctl restart mysql.service'
